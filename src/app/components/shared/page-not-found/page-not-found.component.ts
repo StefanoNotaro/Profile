14ba6d5949339d6d66c403409fd92d32a6054d17
Stefano Notaro <stefano.notaro@gmail.com>
2020-08-09 13:06:52 -0300

    Page not found translation

--- src/app/components/shared/page-not-found/page-not-found.component.ts
+++ src/app/components/shared/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-page-not-found',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./page-not-found.component.css'],
 })
 export class PageNotFoundComponent implements OnInit {
-    constructor() {}
+    constructor(public _translateService: TranslateService) {}
 
     ngOnInit() {}
 }
