165dc045a9127ae4fbee89b7e1a2005ae02118a8
Stefano Notaro Francesco <SNotaro@infragistics.com>
2021-04-15 15:01:35 -0300

    Adding google analytics to page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Globals } from './globals';
 import { TranslateService } from '@ngx-translate/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -10,11 +11,23 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
     title = 'Profile';
-    constructor(public _translateService: TranslateService, _globals: Globals) {
+    // tslint:disable-next-line: variable-name
+    constructor(public _translateService: TranslateService, _globals: Globals, public _router: Router) {
         const languages = _globals.languages.map(x => x.language);
         _translateService.addLangs(languages);
         _translateService.setDefaultLang(languages[0]);
         const browserLanguage = _translateService.getBrowserLang();
         _translateService.use(browserLanguage.match(_globals.languagesRegex) ? browserLanguage : languages[0]);
+
+        // subscribe to router events and send page views to Google Analytics
+        this._router.events.subscribe(event => {
+
+            if (event instanceof NavigationEnd) {
+                (window as any).ga('set', 'page', event.urlAfterRedirects);
+                (window as any).ga('send', 'pageview');
+
+            }
+
+        });
     }
 }
